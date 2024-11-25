f3bd36654a8157525ffe38e031c7fe4a755420a6
saiful6620 <shefa.pust@gmail.com>
2024-11-25 11:44:35 +0600

    Create index.ts
    This is an example with typescript and generics

---
+++ index.ts
@@ -0,0 +1,19 @@
+export const getOptionsWithIdAndName = <T extends Record<string, unknown>>(
+  data: T[] | undefined,
+  idKey: string,
+  nameKey: string,
+): (T & {id: string | number; name: string})[] => {
+  const options: (T & {id: string | number; name: string})[] = []
+  if (data) {
+    data.forEach(item => {
+      const id = item[idKey as keyof T] as string | number
+      const name = item[nameKey as keyof T] as string
+      options.push({
+        ...item,
+        id,
+        name,
+      })
+    })
+  }
+  return options
+}
